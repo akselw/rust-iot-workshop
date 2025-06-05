7ec9fac4fec03d662d59b868821ea496fa79d4ed
Aksel Wester <aksel@wester.co>
2025-06-05 17:03:53 +0200

    Part 1

--- dht-sensor/src/main.rs
+++ dht-sensor/src/main.rs
@@ -1,3 +1,10 @@
+use std::thread::sleep;
+use std::time::Duration;
+use embedded_dht_rs::dht22::Dht22;
+use esp_idf_svc::hal::delay::Delay;
+use esp_idf_svc::hal::gpio::PinDriver;
+use esp_idf_svc::hal::peripherals::Peripherals;
+
 use log::info;
 
 fn main() {
@@ -8,5 +15,24 @@ fn main() {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
+    let peripherals = Peripherals::take().unwrap();
+
+    let pin = PinDriver::input_output_od(peripherals.pins.gpio2).unwrap();
+
+    let mut dht22 = Dht22::new(pin, Delay::default());
+
     info!("Hello, world!");
+    loop {
+        sleep(Duration::from_secs(1));
+
+        match dht22.read() {
+            Ok(sensor_reading) => log::info!(
+                "DHT 22 Sensor - Temperature: {} °C, humidity: {} %",
+                sensor_reading.temperature,
+                sensor_reading.humidity
+            ),
+            Err(error) => log::error!("An error occurred while trying to read sensor: {:?}", error),
+        }
+    }
+
 }
